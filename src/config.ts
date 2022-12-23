7a13b6026d39b95d674ab60111b610c1115fc8d5
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-12-23 15:20:23 +0000

    set auth endpoint to mainnet

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = {
     ADD_TAIL_URL: process.env.REACT_APP_ADD_TAIL_URL || 'https://mainnet-api.taildatabase.com/tail',
     API_URL: process.env.REACT_APP_API_URL || '',
-    AUTH_URL: process.env.REACT_APP_ADD_TAIL_URL || 'http://mainframe.taildatabase.com:8080/auth',
+    AUTH_URL: process.env.REACT_APP_ADD_TAIL_URL || 'https://mainnet-api.taildatabase.com/auth',
     GET_TAIL_URL: process.env.REACT_APP_GET_TAIL_URL || '',
     GET_TAILS_URL: process.env.REACT_APP_GET_TAILS_URL || '',
     GET_SEARCH_INDEX_URL: process.env.REACT_APP_GET_SEARCH_INDEX_URL || '',
